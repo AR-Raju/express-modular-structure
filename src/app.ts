ab1578c404bdfa71e40b6ad5d4688974d36e9489
AR-Raju <asikurrahmanraju@gmail.com>
2024-05-17 08:03:08 +0600

    adding modular code

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import cors from "cors";
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
+import { studentRoutes } from "./app/modules/student/student.route";
 const app: Application = express();
 
 // parser
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+// application routes
+app.use("/api/v1/students", studentRoutes);
 
 export default app;
